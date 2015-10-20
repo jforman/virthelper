976db04aa5dbe92cf32c8c059f60eb9504df6167
jeffrey forman <jeff@jeffreyforman.net>
2015-10-19 21:33:21 -0400

    create coreos VM type, split out ubuntu and debian vm types add post-install script for ubuntu/debian that enables serial console support
    ubuntu is a subtype of debian

--- vmbuilder.py
+++ vmbuilder.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+"""A helpful wrapper for using libvirt to create virtual machines."""
 import argparse
+from bs4 import BeautifulSoup
 import libvirt
 import logging
+import os
 import subprocess
 import sys
 
 import vmtypes
 
 class HandledException(Exception):
+    """General exception for known cases of building VM."""
     pass
 
 class VMBuilder(object):
+    """Class to marshall build of a VM."""
 
     def __init__(self):
         self.args = self.parseArgs()
@@ -20,9 +25,12 @@ class VMBuilder(object):
         self.flags = {}
         self.vm_name = "%s.%s" % (self.args.host_name,
                                   self.args.domain_name)
+        self.vm_disk_image = "%s.qcow2" % self.vm_name
+        self.pool_path = None
 
 
     def configureLogging(self):
+        """Configure logging level."""
         if self.args.debug:
             log_level = logging.DEBUG
         else:
@@ -32,6 +40,7 @@ class VMBuilder(object):
                             format="%(asctime)s %(levelname)s: %(message)s")
 
     def getBuild(self):
+        """Create or return vm builder object."""
         if self.build:
             return self.build
 
@@ -45,6 +54,7 @@ class VMBuilder(object):
         return self.build
 
     def getConn(self):
+        """Create or return libvirt connection to VM host."""
         if self.conn:
             return self.conn
         self.conn = libvirt.open("qemu+ssh://%s/system" % self.args.vm_host)
@@ -52,7 +62,25 @@ class VMBuilder(object):
 
     def getDiskPools(self):
         """Return list of disk pools on VM host."""
-        return [current.name() for current in self.getConn().listAllStoragePools()]
+        return [current.name() for current in
+                self.getConn().listAllStoragePools()]
+
+    def getPoolPath(self):
+        """Return the absolute path for the VM's disk pool."""
+        if self.pool_path:
+            return self.pool_path
+
+        command_line = ["/usr/bin/virsh", "pool-dumpxml",
+                        self.args.disk_pool_name]
+        try:
+            output = subprocess.check_output(command_line,
+                                             stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            logging.error("Error in creating disk image: %s.", err.output)
+            raise
+        soup = BeautifulSoup(output)
+        self.pool_path = soup.target.path.string
+        return self.pool_path
 
     def getPoolVolumes(self, pool):
         """Return list of all volumes in a disk pool."""
@@ -69,7 +97,9 @@ class VMBuilder(object):
         domains = [x.name() for x in self.getConn().listAllDomains()]
         return domains
 
-    def parseArgs(self):
+    @classmethod
+    def parseArgs(cls):
+        """Parse and return command line flags."""
         parser = argparse.ArgumentParser()
         commands = parser.add_argument_group('commands')
         commands.add_argument('command',
@@ -88,10 +118,10 @@ class VMBuilder(object):
         vm_props.add_argument("--disk_size_gb",
                               default=10,
                               type=int,
-                              help="Default size of hard disk image, in GB. Default: %(default)d")
+                              help="Size (GB) of disk image. Default: %(default)d")
         vm_props.add_argument("--domain_name",
                               default="wired.boston.jeffreyforman.net",
-                              help="Domain name the VM upon creation. Default: %(default)s")
+                              help="VM domain name. Default: %(default)s")
         vm_props.add_argument("--memory",
                               type=int,
                               default=512,
@@ -106,13 +136,14 @@ class VMBuilder(object):
         vm_props.add_argument("--host_name",
                               help="Virtual Machine Hostname")
         vm_props.add_argument("--dist_location",
-                              help="URL to installation source. Default: %(default)s",
-                              default="ftp://debian.csail.mit.edu/debian/dists/jessie/main/installer-amd64/")
+                              help="Installation source. URL Default: %(default)s",
+                              default=("ftp://debian.csail.mit.edu/debian/"
+                                       "dists/jessie/main/installer-amd64/"))
 
         vm_host_props = parser.add_argument_group('vm host properties')
         vm_host_props.add_argument("--vm_host",
                                    default="localhost",
-                                   help="VM host system to connect to for creating VM guest. Default: %(default)s")
+                                   help="VM host. Default: %(default)s")
 
         parser.add_argument("--debug",
                             action="store_true",
@@ -122,7 +153,27 @@ class VMBuilder(object):
                             help="Delete VM data store and configuration if it exists.")
         parser.add_argument("--dry_run",
                             action="store_true",
-                            help="Don't actually do anything, but print out what would have been done.")
+                            help=("Don't execute commands, only print out "
+                                  "what would have been done."))
+
+        coreos_args = parser.add_argument_group('coreos vm properties')
+        coreos_args.add_argument("--coreos_channel",
+                                 choices=['stable', 'beta', 'alpha'],
+                                 default='stable',
+                                 help=("Channel of CoreOS image to use as VM base. "
+                                       "Default: %(default)s."))
+        coreos_args.add_argument("--coreos_image_age",
+                                 default=7,
+                                 help=("Age (days) of CoreOS base image before "
+                                       "downloading a new one. Default: %(default)s"))
+        coreos_args.add_argument("--coreos_cloud_config_template",
+                                 default=os.path.join(
+                                     os.path.dirname(
+                                         os.path.realpath(__file__)),
+                                     "coreos_user_data.template"),
+                                 help=("Mako template for CoreOS cloud config "
+                                       "user_data. Default: %(default)s"))
+
         # parser.add_argument("--ip_address",
         #                     default=None,
         #                     help="Static IP address for VM.")
@@ -132,13 +183,13 @@ class VMBuilder(object):
 
     def createDiskImage(self):
         """Create a qcow2 disk image."""
-        # TOOD: Figure out an API-ish way to create a volume since vol-create-as does not support
-        # providing a --connect flag.
+        # TOOD: Figure out an API-ish way to create volume since
+        #  vol-create-as does not support providing a --connect flag
+        #  (["--connect", "qemu+ssh://%s/system" % self.args.vm_host])
 
         command_line = ["/usr/bin/virsh", "vol-create-as"]
-        #command_line.extend(["--connect", "qemu+ssh://%s/system" % self.args.vm_host])
         command_line.extend(["--pool", self.args.disk_pool_name])
-        command_line.extend(["--name", "%s" % self.vm_name])
+        command_line.extend(["--name", self.vm_disk_image])
         command_line.extend(["--capacity", "%dG" % self.args.disk_size_gb])
         command_line.extend(["--format", "qcow2"])
         command_line.extend(["--prealloc-metadata"])
@@ -152,7 +203,7 @@ class VMBuilder(object):
         try:
             # NO shell=true here.
             output = subprocess.check_output(command_line,
-                                           stderr=subprocess.STDOUT)
+                                             stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as err:
             logging.error("Error in creating disk image: %s.", err.output)
             raise
@@ -160,13 +211,14 @@ class VMBuilder(object):
         logging.debug("Disk image creation output: %s", output)
 
     def deleteVMImage(self):
+        """Delete a VM's disk image."""
         logging.debug("Pool %s volumes: %s",
                       self.args.disk_pool_name,
                       self.getPoolVolumes(self.args.disk_pool_name))
 
-        logging.info("Checking if a previous disk image for this host already exists.")
-        if self.vm_name not in self.getPoolVolumes(self.args.disk_pool_name):
-            logging.info("VM image does not exist for this host. Nothing to delete.")
+        logging.info("Checking for pre-existing disk image for this VM.")
+        if self.vm_disk_image not in self.getPoolVolumes(self.args.disk_pool_name):
+            logging.info("VM image does not exist for VM. Nothing to delete.")
             return
 
         logging.info("Attempting to delete image in pool %s for vm %s.",
@@ -181,10 +233,11 @@ class VMBuilder(object):
                           "not passed.")
             raise HandledException
         self.getConn().storagePoolLookupByName(
-            self.args.disk_pool_name).storageVolLookupByName(self.vm_name).delete()
+            self.args.disk_pool_name).storageVolLookupByName(self.vm_disk_image).delete()
         logging.info("Finished deleting VM image for VM.")
 
     def deleteVM(self):
+        """Stop and delete the VM."""
         if self.args.dry_run:
             logging.info("DRY RUN: VM would have been deleted here.")
             return
@@ -195,7 +248,8 @@ class VMBuilder(object):
 
         logging.info("Found existing VM with same name.")
         if not self.args.deleteifexists:
-            logging.error("VM image found, but --deleteifexists flag not passed.")
+            logging.error("VM image found, but --deleteifexists "
+                          "flag not passed.")
             raise HandledException
 
         if self.getConn().lookupByName(self.vm_name).isActive():
@@ -213,14 +267,17 @@ class VMBuilder(object):
         self.deleteVMImage()
 
     def executeVirtInstall(self):
+        """Execute virt-install with vm-specific flags."""
         command_line = ["/usr/bin/virt-install", "--autostart",
-                        "--nographics"]
-                        #'--console pty,target_type=serial']
+                        "--nographics",
+                        '--console pty,target_type=serial']
+        if self.args.debug:
+            command_line.extend(["--debug"])
+
         self.flags.update({
             "connect": "qemu+ssh://%s/system" % self.args.vm_host,
             "disk": "vol=%s/%s,cache=none" % (self.args.disk_pool_name,
-                                              self.vm_name),
-            "location": self.args.dist_location,
+                                              self.vm_disk_image),
             "name": self.vm_name,
             "network": "bridge=%s" % self.args.bridge_interface,
             "os-type": "linux",
@@ -229,8 +286,10 @@ class VMBuilder(object):
             "virt-type": "kvm",
         })
 
-        extra_args = self.getBuild().getExtraArgs()
+        self.flags.update(self.getBuild().getVirtInstallCustomFlags())
+        extra_args = self.getBuild().getVirtInstallExtraArgs()
         if extra_args:
+            logging.debug("Found extra-args for virt-install.")
             self.flags.update({'extra-args': extra_args})
 
         for flag, value in self.flags.iteritems():
@@ -245,11 +304,14 @@ class VMBuilder(object):
             logging.info("DRYRUN: VM not actually created. Skipping.")
             return
 
+        self.getBuild().executePreVirtInstall()
         subprocess.call(str_command_line,
                         stderr=subprocess.STDOUT,
                         shell=True)
+        self.getBuild().executePostVirtInstall()
 
     def createVM(self):
+        """Main execution handler for the script."""
         logging.info("Starting VM build for %s.%s.", self.args.host_name,
                      self.args.domain_name)
 
--- vmtypes.py
+++ vmtypes.py
@@ -1,110 +1,274 @@
+"""VM-specific configuration for using vmbuilder wrapper."""
 import logging
+import os
+import subprocess
+import time
+import urllib
 
-import vmbuilder
+from mako.template import Template
 
-class CoreOS(vmbuilder.VMBuilder):
+from vmbuilder import VMBuilder, HandledException
 
-  def __init__(self):
-      super(CoreOS, self).__init__()
+class BaseVM(VMBuilder):
+    """Base class for all VM types."""
 
-class Debian(vmbuilder.VMBuilder):
+    def __init__(self):
+        super(BaseVM, self).__init__()
 
-  def __init__(self):
-      super(Debian, self).__init__()
+    def executePreVirtInstall(self):
+        """Logic to execute right before running virt-install."""
+        pass
 
-  def getExtraArgs(self):
-    extra_args = {
-      "keyboard-configuration/xkb-keymap": "us",
-      "console-setup/ask_detect": "false",
-      "locale": "en_US", #.UTF-8",
-      "netcfg/get_domain": self.args.domain_name,
-      "netcfg/get_hostname": self.args.host_name,
-      "preseed/url": "http://10.10.0.1/jf-custom-debian.preseed"
-      }
+    def executePostVirtInstall(self):
+        """Logic to execute right after running virt-install."""
+        pass
 
-    # Order of console specs is important. tt0, ttyS0
-    add_ons = ['serial', 'console=tty0', 'console=ttyS0,9600n8']
+    def getVirtInstallExtraArgs(self):
+        """Construct VM-type specific extra-args flag for virt-install
 
-    # NOTE: This needs to be reimplemented.
-    # if self.args.ip_address:
-    #   network_info = NETWORK_CONFIG[self.args.bridge_interface]
-    #   if not self.args.ip_address.startswith(network_info['network']):
-    #     logging.error("You assigned an IP address (%s) that does not match the requested interface (%s).", args.ip_address,
-    #                   self.args.bridge_interface)
-    #   raise vmbuilder.HandledException
-    #
-    #   extra_args += (" " +
-    #                "netcfg/get_nameservers=" + network_info['nameserver'] + " " +
-    #                "netcfg/get_ipaddress=" + self.args.ip_address + " " +
-    #                "netcfg/get_netmask=" + self.network_info['netmask'] + " " +
-    #                "netcfg/get_gateway=" + self.network_info['gateway'] + " " +
-    #                "netcfg/confirm_static=true " +
-    #                "netcfg/disable_autoconfig=true")
-    #
-    # custom_flags += "\""
+        Takes a dict of (key, value) and returns
+        "key=value,key=value,add_on1,add_on2" string
+        """
+        pass
 
-    result = []
-    for key,value in extra_args.iteritems():
-      result.append("%s=%s" % (key, value))
-    result = " ".join(result)
+    def getVirtInstallCustomFlags(self):
+        """Custom flags to append to the virt-install execution."""
+        pass
 
-    for current in add_ons:
-      result += " %s" % current
 
-    result = "\"%s\"" % result
+class CoreOS(BaseVM):
+    """CoreOS VM base class."""
 
-    return result
-      
+    def __init__(self):
+        super(CoreOS, self).__init__()
+        # Compressed local snapshot image path
+        self.comp_local_snapshot_image_path = "%s/coreos_production_qemu_image-%s.img.bz2" % (
+            self.getPoolPath(),
+            self.args.coreos_channel)
+        self.uncomp_local_snapshot_image_path = "%s/coreos_production_qemu_image-%s.img" % (
+            self.getPoolPath(),
+            self.args.coreos_channel)
+
+    def _getCloudConfigPath(self):
+        """Return absolute path to VM's cloud config file."""
+        return os.path.join(self.getPoolPath(), "coreos",
+                            self.vm_name, "openstack", "latest", "user_data")
+
+    def deleteVM(self):
+        super(CoreOS, self).deleteVM()
+        logging.info("Deleting CoreOS config drive for %s.", self.vm_name)
+        cloud_config_path = "%s/coreos/%s/openstack/latest" % (self.getPoolPath(), self.vm_name)
+        if os.path.exists(cloud_config_path):
+            if self.args.dry_run:
+                logging.info("DRY RUN: Would have attempted to remove cloud config"
+                             " path: %s", cloud_config_path)
+                return
+
+            if self.args.deleteifexists:
+                logging.info("Attempting remote cloud config path for %s: %s", self.vm_name,
+                             cloud_config_path)
+                os.removedirs(cloud_config_path)
+            else:
+                logging.info("Tried to remove %s path, but --deleteifexists flag not "
+                             "passed.", cloud_config_path)
+                raise HandledException
+
+    def normalizeVMState(self):
+        super(CoreOS, self).normalizeVMState()
+        coreos_repo_image = "http://%s.release.core-os.net/amd64-usr/current/coreos_production_qemu_image.img.bz2" % self.args.coreos_channel
+        logging.debug("Determining if a new CoreOS snapshot image needs to be downloaded.")
+        if os.path.exists(self.comp_local_snapshot_image_path):
+            snapshot_ctime = os.path.getctime(self.comp_local_snapshot_image_path)
+            logging.debug("snapshot ctime: %s", snapshot_ctime)
+            now = time.time()
+            threshold = self.args.coreos_image_age * (24 * 60 * 60)
+            if (now - snapshot_ctime) < threshold:
+                logging.info("CoreOS snapshot image is less than %s days old. "
+                             "Not re-downloading.", self.args.coreos_image_age)
+                return
+
+            message = ("It has been more than %s days since redownloading CoreOS %s image. "
+                       "Let's delete the old one, and grab a new one." % (
+                           self.args.coreos_image_age, self.args.coreos_channel))
+            if self.args.dry_run:
+                logging.info("DRY RUN: Would have deleted old image here.")
+            else:
+                os.remove(self.comp_local_snapshot_image_path)
+                os.remove(self.uncomp_local_snapshot_image_path)
+        else:
+            message = "No local CoreOS %s image was found. Need to download." % self.args.coreos_channel
+
+        logging.info(message)
+        if self.args.dry_run:
+            logging.info("DRY RUN: Would have retrieved a new %s CoreOS image here.",
+                         self.args.coreos_channel)
+            return
+
+        logging.info("Attempting to download %s to %s.",
+                     coreos_repo_image, self.comp_local_snapshot_image_path)
+        urllib.urlretrieve(coreos_repo_image,
+                           self.comp_local_snapshot_image_path)
+        logging.info("Finished download of %s to %s",
+                     coreos_repo_image,
+                     self.comp_local_snapshot_image_path)
+
+        logging.debug("Decompressing CoreOS image: %s.", self.comp_local_snapshot_image_path)
+        subprocess.check_call(["/bin/bzip2", "-d", "-k", self.comp_local_snapshot_image_path])
+        logging.info("Done decompressing CoreOS image.")
+
+    def createDiskImage(self):
+        """Create a qcow2 disk image using CoreOS snapshot image."""
+
+        vm_image_path = "%s/%s" % (self.getPoolPath(), self.vm_disk_image)
+
+        commands = []
+        command_line = ["/usr/bin/qemu-img", "create", "-f", "qcow2"]
+        command_line.extend(["-b", self.uncomp_local_snapshot_image_path])
+        command_line.extend([vm_image_path])
+        logging.debug("qemu-img command line: %s", " ".join(command_line))
+        commands.extend([command_line])
+
+        command_line = ["/usr/bin/virsh", "pool-refresh", "--pool", self.args.disk_pool_name]
+        commands.extend([command_line])
+
+        command_line = ["/usr/bin/virsh", "vol-upload", "--vol", os.path.basename(vm_image_path),
+                        "--pool", self.args.disk_pool_name, "--file", vm_image_path]
+        commands.extend([command_line])
+
+        try:
+            # NO shell=true here.
+            logging.info("Creating CoreOS VM disk image.")
+            for current in commands:
+                logging.debug("executing: %s", " ".join(current))
+                if self.args.dry_run:
+                    logging.info("DRY RUN: Did not actually execute.")
+                    continue
+                output = subprocess.check_output(current,
+                                                 stderr=subprocess.STDOUT)
+                logging.info("Disk image created successfully.")
+                logging.debug("Disk image creation output: %s", output)
+        except subprocess.CalledProcessError as err:
+            logging.error("Error in creating disk image: %s.", err.output)
+            raise HandledException
+
+    def getVirtInstallCustomFlags(self):
+        extra_args = {
+            "os-variant": "virtio26",
+            "import": "",
+            "filesystem": "%s,config-2,type=mount,mode=squash" % os.path.join(
+                self.getPoolPath(), "coreos", self.vm_name),
+        }
+        return extra_args
+
+    def executePreVirtInstall(self):
+        self.writeCloudConfig()
+
+
+    def writeCloudConfig(self):
+        """Write VM's cloud config data to file."""
+        if not os.path.exists(os.path.dirname(self._getCloudConfigPath())):
+            os.makedirs(os.path.dirname(self._getCloudConfigPath()))
+
+        template = Template(filename=self.args.coreos_cloud_config_template)
+        cloud_config_vars = {
+            'vm_name': self.vm_name,
+        }
+        template_rendered = template.render(**cloud_config_vars)
+        with open(self._getCloudConfigPath(), "w") as cloud_config:
+            cloud_config.write(template_rendered)
+        logging.debug("rendered cloud config %s", template_rendered)
+
+class Debian(BaseVM):
+    """Debian-specific configuration for VM installation.
+
+    Includes Ubuntu as a subclass.
+    """
+
+    def __init__(self):
+        super(Debian, self).__init__()
+
+    def getVirtInstallCustomFlags(self):
+        return {
+            "location": self.args.dist_location,
+        }
+
+    def getVirtInstallExtraArgs(self):
+        extra_args = {
+            "keyboard-configuration/xkb-keymap": "us",
+            "console-setup/ask_detect": "false",
+            "locale": "en_US", #.UTF-8",
+            "netcfg/get_domain": self.args.domain_name,
+            "netcfg/get_hostname": self.args.host_name,
+            "preseed/url": "http://10.10.0.1/jf-custom-debian.preseed"
+        }
+
+        add_ons = ['serial', 'console=tty0', 'console=ttyS0,9600n8']
+        result = []
+        for key, value in extra_args.iteritems():
+            result.append("%s=%s" % (key, value))
+        result = " ".join(result)
+
+        for current in add_ons:
+            result += " %s" % current
+
+        result = "\"%s\"" % result
+
+        return result
+
+    def getNetworkExtraArgs(self):
+        """WIP: Debian-specific extra args for networking."""
+        # NOTE: This needs to be reimplemented.
+        # if self.args.ip_address:
+        #   network_info = NETWORK_CONFIG[self.args.bridge_interface]
+        #   if not self.args.ip_address.startswith(network_info['network']):
+        #     logging.error("You assigned an IP address (%s) that does not match the requested interface (%s).", args.ip_address,
+        #                   self.args.bridge_interface)
+        #   raise vmbuilder.HandledException
+        #
+        #   extra_args += (" " +
+        #                "netcfg/get_nameservers=" + network_info['nameserver'] + " " +
+        #                "netcfg/get_ipaddress=" + self.args.ip_address + " " +
+        #                "netcfg/get_netmask=" + self.network_info['netmask'] + " " +
+        #                "netcfg/get_gateway=" + self.network_info['gateway'] + " " +
+        #                "netcfg/confirm_static=true " +
+        #                "netcfg/disable_autoconfig=true")
+        #
+        # custom_flags += "\""
+        pass
+
+
 class Ubuntu(Debian):
+    """Ubuntu-specific configuration for VM install."""
 
-  def __init__(self):
-      super(Ubuntu, self).__init__()
+    def __init__(self):
+        super(Ubuntu, self).__init__()
 
-  def getExtraArgs(self):
+    def getVirtInstallExtraArgs(self):
+        extra_args = {
+            "keyboard-configuration/xkb-keymap": "us",
+            #"console-keymaps-at/keymap": "us",
+            "console-keymaps-at/keymap": "American",
+            "console-setup/ask_detect": "false",
+            "console-setup/layoutcode": "us",
+            "keyboard-configuration/layout": "USA",
+            "keyboard-configuration/variant": "US",
+            "locale": "en_US", #.UTF-8",
+            "netcfg/get_domain": self.args.domain_name,
+            "netcfg/get_hostname": self.args.host_name,
+            "preseed/url": "http://10.10.0.1/jf-custom-ubuntu.preseed"
+        }
 
-    extra_args = {
-      "keyboard-configuration/xkb-keymap": "us",
-      #"console-keymaps-at/keymap": "us",
-      "console-keymaps-at/keymap": "American",
-      "console-setup/ask_detect": "false",
-      "console-setup/layoutcode": "us",
-      "keyboard-configuration/layout": "USA",
-      "keyboard-configuration/variant": "US",
-      "locale": "en_US", #.UTF-8",
-      "netcfg/get_domain": self.args.domain_name,
-      "netcfg/get_hostname": self.args.host_name,
-      "preseed/url": "http://autobahn.jeffreyforman.net/jf-custom-debian.preseed"
-      }
+        add_ons = ['serial', 'console=tty0', 'console=ttyS0,9600n8']
 
-    # Order of console specs is important. tt0, ttyS0
-    add_ons = ['serial', 'console=tty0', 'console=ttyS0,9600n8']
+        # Get networkextra args working.
+        network_extra_args = self.getNetworkExtraArgs()
+        result = []
+        for key, value in extra_args.iteritems():
+            result.append("%s=%s" % (key, value))
+        result = " ".join(result)
 
-    # NOTE: This needs to be reimplemented.
-    # if self.args.ip_address:
-    #   network_info = NETWORK_CONFIG[self.args.bridge_interface]
-    #   if not self.args.ip_address.startswith(network_info['network']):
-    #     logging.error("You assigned an IP address (%s) that does not match the requested interface (%s).", args.ip_address,
-    #                   self.args.bridge_interface)
-    #   raise vmbuilder.HandledException
-    #
-    #   extra_args += (" " +
-    #                "netcfg/get_nameservers=" + network_info['nameserver'] + " " +
-    #                "netcfg/get_ipaddress=" + self.args.ip_address + " " +
-    #                "netcfg/get_netmask=" + self.network_info['netmask'] + " " +
-    #                "netcfg/get_gateway=" + self.network_info['gateway'] + " " +
-    #                "netcfg/confirm_static=true " +
-    #                "netcfg/disable_autoconfig=true")
-    #
-    # custom_flags += "\""
+        for current in add_ons:
+            result += " %s" % current
+        result = "\"%s\"" % result
 
-    result = []
-    for key,value in extra_args.iteritems():
-      result.append("%s=%s" % (key, value))
-    result = " ".join(result)
-
-    for current in add_ons:
-      result += " %s" % current
-
-    result = "\"%s\"" % result
-
-    return result
+        return result
