01d5596b174a84da5ceddc424d445a633c39c02e
Jeffrey Forman <jforman@gmail.com>
2020-05-08 14:52:40 -0400

    refactor normalizeVmState a bit to deal with no VMs present in the cluster. create a function to to get all the VM info from all nodes at first

--- proxmox_ubuntu_cloud.py
+++ proxmox_ubuntu_cloud.py
@@ -18,26 +18,41 @@ class ProxmoxUbuntuCloud(vmtypes.BaseVM):
             user=self.args.proxmox_username,
             password=self.args.proxmox_password,
             verify_ssl=False)
+        self.allvminfo = {}
+        self.getAllVMInfo()
 
-
-    def normalizeVMState(self):
-        """iterate over all nodes, delete VM if one of same name is found."""
-        vm_dict = {}
+    def getAllVMInfo(self):
+        """Make a dict containing information on all VMs."""
+        logging.info("Creating dict of all VM info.")
+        if self.allvminfo:
+            return self.allvminfo
         for node in self.proxmox.nodes.get():
             logging.debug(f"Looking for VMs on node {node['node']}.")
             for vm in self.proxmox.nodes(node['node']).qemu.get():
                 logging.debug(f"Found VM: {vm['name']}.")
-                if self.getVmName() == vm['name']:
-                    logging.info(f"Found a VM by {self.getVmName()} already exists.")
-                    if self.args.deleteifexists:
-                        logging.info("Deleting existing VM.")
-                        self.proxmox.nodes(node['node']).qemu(vm['vmid']).delete()
-                        logging.debug("Finished deleting VM.")
-                        return
-                    else:
-                        logging.critical(
-                            "Existing VM by that name already found, but --deleteifexists flag "
-                            "not passed. Exiting.")
-                        sys.exit(1)
-        logging.debug("Done with normalizeVmState.")
+                self.allvminfo[vm['name']] = vm
+                self.allvminfo[vm['name']]['node'] = node
+        logging.info(f"Found {len(self.allvminfo)} VMs.")
+        logging.info("Done creating dict of all VM info.")
 
+    def normalizeVMState(self):
+        """Delete VM if one of same name is found."""
+        logging.info(f"Normalizing VM state for VM {self.getVmName()}.")
+        vm_dict = {}
+        if not bool(self.getAllVMInfo()):
+            # No VMs are found in the cluster.
+            return
+
+        if self.getVmName() in self.getAllVMInfo().keys():
+            logging.info(f"Found a VM by {self.getVmName()} already exists.")
+            if self.args.deleteifexists:
+                logging.info("Deleting existing VM.")
+                self.proxmox.nodes(node['node']).qemu(vm['vmid']).delete()
+                logging.debug("Finished deleting VM.")
+                return
+            else:
+                logging.critical(
+                    "Existing VM by that name already found, but --deleteifexists flag "
+                    "not passed. Exiting.")
+                sys.exit(1)
+        logging.debug("Done with normalizeVmState.")
